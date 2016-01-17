d79e7eb9405598c83691aa262806166b6b5dc33f
sighalt <roessler@sighalt.de>
2016-01-17 19:53:20 +0100

    added docstrings

--- pyhooker/func_utils.py
+++ pyhooker/func_utils.py
@@ -5,6 +5,12 @@ __author__ = 'sighalt'
 
 
 def build_extensive_kwargs(func, args, kwargs):
+    """
+    Return a dictionary of the merged args list and kwargs dictionary.
+
+    This method will chose the right method of merging based on the python version.
+    :rtype: dict
+    """
 
     if sys.version_info >= (3, 3):
         return _build_extensive_kwargs_with_signature(func, args, kwargs)
@@ -13,6 +19,13 @@ def build_extensive_kwargs(func, args, kwargs):
 
 
 def _build_extensive_kwargs_with_signature(func, args, kwargs):
+    """
+    Return a dictionary of the merged args list and kwargs dictionary by using inspect.signature().
+
+    This method only works in python >= 3.3.
+
+    :rtype: dict
+    """
     function_signature = inspect.signature(func)
     extensive_kwargs = function_signature.bind_partial(*args, **kwargs)
 
@@ -20,19 +33,26 @@ def _build_extensive_kwargs_with_signature(func, args, kwargs):
 
 
 def _build_extensive_kwargs_with_getargspec(func, args, kwargs):
-        args = list(args)
-        obj_arguments = inspect.getargspec(func).args
-        extensive_kwargs = {}
+    """
+    Return a dictionary of the merged args list and kwargs dictionary by using inspect.getargspec().
 
-        for argument_name in obj_arguments:
+    This method is deprecated in python >= 3.3.
+
+    :rtype: dict
+    """
+    args = list(args)
+    obj_arguments = inspect.getargspec(func).args
+    extensive_kwargs = {}
+
+    for argument_name in obj_arguments:
+        try:
+            # try to provide this argument from args
+            extensive_kwargs[argument_name] = args.pop(0)
+        except IndexError:
+            # try to provide this argument from kwargs
             try:
-                # try to provide this argument from args
-                extensive_kwargs[argument_name] = args.pop(0)
-            except IndexError:
-                # try to provide this argument from kwargs
-                try:
-                    extensive_kwargs[argument_name] = kwargs[argument_name]
-                except KeyError:
-                    continue
+                extensive_kwargs[argument_name] = kwargs[argument_name]
+            except KeyError:
+                continue
 
-        return extensive_kwargs
+    return extensive_kwargs
